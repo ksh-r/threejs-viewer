b44c15919316f007a23e90f48d24b5815f421b87
Keshar Wakharkar <201851155@iiitvadodara.ac.in>
2021-06-01 20:25:37 +0530

    Added gltfpack compressed models

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-
+import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module';
+// Compression with gltfpack, command -> gltfpack -i mwuvn.gltf -o mwuvnPacked.glb -cc
 
 export default class Sketch {
     constructor(options) {
@@ -14,7 +15,7 @@ export default class Sketch {
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
 
-        this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 0.01, 200);
+        this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 0.01, 500);
         this.camera.position.z = 5;
 
         this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -29,20 +30,22 @@ export default class Sketch {
         this.render();
     }
     addObjects() {
+
         // this.geo = new THREE.BoxBufferGeometry(1,1,1,5,5,5);
         // this.mat = new THREE.MeshNormalMaterial();
         // this.cube = new THREE.Mesh(this.geo, this.mat);
+        // this.cube.position.set(-5,-5,-5)
         // this.scene.add(this.cube);
 
         var loader = new GLTFLoader();
+        loader.setMeshoptDecoder(MeshoptDecoder);
         var dracoLoader = new DRACOLoader();
         dracoLoader.setDecoderPath('../src/draco-files/');
         loader.setDRACOLoader(dracoLoader);
-        loader.load('../src/models/skull.glb', (gltf) => {
+        loader.load('../src/models/skullNormals.glb', (gltf) => {
             gltf.scene.traverse(function (child) {
                 if (child.isMesh) {
                     child.material = new THREE.MeshNormalMaterial();
-                    // child.scale.set(0.1, 0.1, 0.1);
                 }
             });
             this.scene.add(gltf.scene);
